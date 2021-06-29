6d5437b7d48f7cecde0bcad59b2ff766d74d635c
Dariusz Smigiel <1412617+dasm@users.noreply.github.com>
2021-06-29 15:26:40 -0700

    Working solution

--- src/charm.py
+++ src/charm.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env python3
-# Copyright 2021 dasm
-# See LICENSE file for licensing details.
-
 
 import logging
 from collections import OrderedDict
@@ -11,69 +8,57 @@ from charms.nginx_ingress_integrator.v0.ingress import IngressRequires
 from ops.charm import CharmBase, ConfigChangedEvent, PebbleReadyEvent, UpdateStatusEvent
 from ops.framework import StoredState
 from ops.main import main
-from ops.model import ActiveStatus, WaitingStatus, ModelError
-from ops.pebble import ConnectionError, ServiceStatus, ChangeError
+from ops.model import ActiveStatus, BlockedStatus, MaintenanceStatus
+
 
 logger = logging.getLogger(__name__)
-
-
-OPENSEARCH_ARGS = OrderedDict(
-    JVM_HEAP_SIZE="jvm-heap-size",
-    CLUSTER_NAME="cluster-name",
-)
 
 
 class CharmOpenSearch(CharmBase):
     """Charm the service."""
 
-    state = StoredState()
+    stored = StoredState()
 
     def __init__(self, *args):
         super().__init__(*args)
-        self.framework.observe(
-            self.on.opensearch_pebble_ready, self._on_opensearch_pebble_ready
-        )
-        self.framework.observe(self.on.config_changed, self._on_config_changed)
-        self.framework.observe(self.on.update_status, self._on_update_status)
-
-        self.state.set_default(auto_start=True)
-        self.state.set_default(jvm_heap_size="512m")
-        self.state.set_default(cluster_name="opensearch-cluster")
-        self.state.set_default(node_name="opensearch-node1")
-        self.state.set_default(discovery_seed_hosts="opensearch-node1")
-        self.state.set_default(cluster_initial_master_nodes="opensearch-node1")
+        self.framework.observe(self.on.opensearch_pebble_ready, self._on_pebble_ready)
+        #self.framework.observe(self.on.config_changed, self._on_config_changed)
 
         self.ingress = IngressRequires(
             self,
             {
-                "service-hostname": "opensearch.juju",
+                "service-hostname": self.config["node_name"],
                 "service-name": self.app.name,
                 "service-port": 9200,
             },
         )
 
     def _opensearch_layer(self):
-        environment = {
-            "OPENSEARCH_JAVA_OPTS": f"-Xms{self.state.jvm_heap_size} -Xmx{self.state.jvm_heap_size}",
-        }
-
-        # Install missing "su"
+        cluster_name = self.config["cluster_name"]
+        node_name = self.config["node_name"]
+        seed = self.config["discovery_seed_hosts"]
+        cluster_type = self.config["type"]
 
         cmd = (
-            'yum install -y procps; su -p opensearch -c "/usr/share/opensearch/bin/opensearch '
-            f"-Ecluster.name={self.state.cluster_name} "
-            f"-Enode.name={self.state.node_name} "
-            f"-Ediscovery.seed_hosts={self.state.discovery_seed_hosts} "
-            f'-Ecluster.initial_master_nodes={self.state.cluster_initial_master_nodes}"'
+            "/usr/share/opensearch/bin/opensearch "
+            f"-Ecluster.name={cluster_name} "
+            f"-Enode.name={node_name} "
+            f"-Ediscovery.seed_hosts={seed} "
+            f"-Ediscovery.type={cluster_type}"
         )
+
+        jvm_heap_size = self.config["jvm_heap_size"]
 
         return {
             "summary": "opensearch layer",
             "description": "pebble config layer for opensearchproject/opensearch",
             "services": {
                 "opensearch": {
+                    "user": "opensearch",
                     "override": "merge",
-                    "environment": environment,
+                    "environment": {
+                        "OPENSEARCH_JAVA_OPTS": f"-Xms{jvm_heap_size} -Xmx{jvm_heap_size}"
+                    },
                     "summary": "opensearch",
                     "command": cmd,
                     "startup": "enabled",
@@ -81,45 +66,31 @@ class CharmOpenSearch(CharmBase):
             },
         }
 
-    def _on_opensearch_pebble_ready(self, event: PebbleReadyEvent) -> None:
+    def _on_pebble_ready(self, event: PebbleReadyEvent) -> None:
         container = event.workload
+
         layer = self._opensearch_layer()
         container.add_layer("opensearch", layer, combine=True)
-        container.start("opensearch")
-        self.unit.status = ActiveStatus()
-
-    def _on_update_status(self, event: UpdateStatusEvent) -> None:
-        pass
+        container.autostart()
+        self.unit.status = ActiveStatus("ready")
 
     def _on_config_changed(self, event: ConfigChangedEvent) -> None:
         container = self.unit.get_container("opensearch")
-        try:
-            service = container.get_service("opensearch")
-        except ConnectionError:
-            logger.info("Pebble API not yet ready, waiting...")
-            return
-        except ModelError:
-            logger.info("Service 'opensearch' not yet defined, waiting...")
-            return
-
-        environment = {}
-        for env_name, config in OPENSEARCH_ARGS.items():
-            cfg = self.config[config]
-            if cfg:
-                environment[env_name] = cfg
-
         layer = self._opensearch_layer()
-        plan = container.get_plan()
-        if plan.services["environment"] != layer["services"]["environment"]:
-            container.add_layer("opensearch", layer, combine=True)
-            logging.debug("Added config layer to Pebble plan")
+        services = container.get_plan().to_dict().get("services", {})
 
-            if service.is_running():
+        config_changed = bool(services != layer["services"])
+
+        if config_changed:
+            container.add_layer("opensearch", layer, combine=True)
+            logging.info("Added updated layer 'opensearch' to Pebble plan")
+
+            if container.get_service("opensearch").is_running():
                 container.stop("opensearch")
-            container.start("opensearch")
-            logging.info("Restarted 'opensearch' service")
-        self.unit.status = ActiveStatus()
+                container.start("opensearch")
+                logging.info("Restarted opensearch service")
+        self.unit.status = ActiveStatus("ready")
 
 
 if __name__ == "__main__":
-    main(CharmOpenSearch)
+    main(CharmOpenSearch, use_juju_for_storage=True)
